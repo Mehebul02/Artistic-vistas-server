0cc5e321499b0f2937e51a0fb4dfdcb81e9757dd
Mehebul02 <mehebul2123@gmail.com>
2024-04-28 03:02:31 +0600

    My Crafts added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 // }
 // app.use(cors(corsConfig))
 // app.options("", cors(corsConfig))
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.po42dna.mongodb.net/?retryWrites=true&w=majority`;
@@ -33,17 +33,24 @@ async function run() {
     // await client.connect();
     const database = client.db("craftsDB");
     const craftsCollection = database.collection("crafts");
-    app.get('/crafts',async(req,res)=>{
-      const query = craftsCollection.find()
-      const result = await query.toArray()
-      res.send(result)
-    })
+    app.get("/crafts", async (req, res) => {
+      const query = craftsCollection.find();
+      const result = await query.toArray();
+      res.send(result);
+    });
     app.get("/crafts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await craftsCollection.findOne(query);
       res.send(result);
     });
+//  my Craft add list 
+    app.get('/myCrafts/:email',async(req,res)=>{
+      console.log(req.params.email)
+      const result =await craftsCollection.find({email:req.params.email}).toArray()
+      res.send(result)
+    })
+    
 
     app.post("/crafts", async (req, res) => {
       const craft = req.body;
